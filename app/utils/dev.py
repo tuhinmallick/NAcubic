02915383f3aefb5283431dc785c2679601522398
Tuhin Mallick <tuhin.mllk@gmail.com>
2023-01-24 22:36:18 +0000

    Removed redundant package

--- app/utils/dev.py
+++ app/utils/dev.py
@@ -1,19 +1,19 @@
-from functools import wraps
-from time import time
+# from functools import wraps
+# from time import time
 
-from loguru import logger
+# from loguru import logger
 
 
-def timing(func):
-    """Use timing decorator to time functions for debugging."""
+# def timing(func):
+#     """Use timing decorator to time functions for debugging."""
 
-    @wraps(func)
-    def _time_it(*args, **kwargs):
-        start = int(round(time() * 1000))
-        try:
-            return func(*args, **kwargs)
-        finally:
-            end_ = int(round(time() * 1000)) - start
-            logger.debug(f"{func.__name__}:: Total execution time: {max(end_, 0)} ms")
+#     @wraps(func)
+#     def _time_it(*args, **kwargs):
+#         start = int(round(time() * 1000))
+#         try:
+#             return func(*args, **kwargs)
+#         finally:
+#             end_ = int(round(time() * 1000)) - start
+#             logger.debug(f"{func.__name__}:: Total execution time: {max(end_, 0)} ms")
 
-    return _time_it
+#     return _time_it
