7bc7e9edc18bb6ed81941749ce86c4383a4b1e1b
Tuhin Mallick <tuhin.mllk@gmail.com>
2023-01-24 12:16:45 +0000

    d

--- app/dashboard.py
+++ app/dashboard.py
@@ -2,10 +2,36 @@ import streamlit as st
 import cv2
 from PIL import Image
 
-uploaded_video = st.file_uploader("Choose video", type=["mp4", "mov"])
-frame_skip = 300 # display every 300 frames
+uploaded_video = st.sidebar.file_uploader("Choose video", type=["mp4", "avi"])
+if uploaded_video is not None:
+    video_bytes = uploaded_video.read()
+    st.video(uploaded_video,format="video/x-msvideo")
 
-if uploaded_video is not None: # run only when user uploads video
+
+
+
+
+
+   
+
+frame_skip = 300 # display every 300 frames
+window_size =  st.sidebar.number_input("Window Size", value = 12)
+roi =  st.sidebar.checkbox("ROI analysis")
+process_image_stack = st.sidebar.button("Process Image Stack")
+signal_to_noise_ratio =  st.sidebar.number_input("Signal to Noise Ratio", value = 3)
+signal_average_threshold =  st.sidebar.number_input("Signal Average Threshold", value = 15)
+general_activity_tracker =  st.sidebar.checkbox("General Activity Tracker")
+include_variance =  st.sidebar.checkbox("Include Variance")
+if include_variance:
+    variance =  st.sidebar.number_input("Variance value", value = 1)
+minimum_activity_counts =  st.sidebar.number_input("Minimum activity counts", value = 1)
+col1, col2 = st.sidebar.columns(2)
+with col1:
+    detect_activity = st.sidebar.button("Detect Activty")
+with col2:
+    reset = st.sidebar.button("Reset")
+
+if uploaded_video is not None and process_image_stack: # run only when user uploads video
     vid = uploaded_video.name
     with open(vid, mode='wb') as f:
         f.write(uploaded_video.read()) # save video to disk
