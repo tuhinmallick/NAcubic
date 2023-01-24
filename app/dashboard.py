c2f712a553213d08cba0daee7664328102ecb1f8
Tuhin Mallick <tuhin.mllk@gmail.com>
2023-01-24 10:53:15 +0000

    dD

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,51 +1,29 @@
-"""Main module for the streamlit app"""
-
-import numpy as np
-import pandas as pd
 import streamlit as st
-from pages.about import About
-from pages.datatable import DataTable
-from utils.sidebar import sidebar_caption
+import cv2
+from PIL import Image
 
-# Config the whole app
-st.set_page_config(
-    page_title="A Dashboard Template",
-    page_icon="🧊",
-    layout="wide",
-    initial_sidebar_state="expanded",
-)
+uploaded_video = st.file_uploader("Choose video", type=["mp4", "mov"])
+frame_skip = 300 # display every 300 frames
 
+if uploaded_video is not None: # run only when user uploads video
+    vid = uploaded_video.name
+    with open(vid, mode='wb') as f:
+        f.write(uploaded_video.read()) # save video to disk
 
-@st.cache()
-def fake_data():
-    """some fake data"""
+    st.markdown(f"""
+    ### Files
+    - {vid}
+    """,
+    unsafe_allow_html=True) # display file name
 
-    dt = pd.date_range("2021-01-01", "2021-03-01")
-    return pd.DataFrame(
-        {"datetime": dt, "values": np.random.randint(0, 10, size=len(dt))}
-    )
+    vidcap = cv2.VideoCapture(vid) # load video from disk
+    cur_frame = 0
+    success = True
 
-
-def main():
-    """A streamlit app template"""
-
-    st.sidebar.title("Tools")
-
-    PAGES = {"Table": DataTable, "About": About}
-
-    # Select pages
-    # Use dropdown if you prefer
-    selection = st.sidebar.radio("Pages", list(PAGES.keys()))
-    sidebar_caption()
-
-    page = PAGES[selection]
-
-    DATA = {"base": fake_data()}
-
-    with st.spinner(f"Loading Page {selection} ..."):
-        page = page(DATA)
-        page()
-
-
-if __name__ == "__main__":
-    main()
+    while success:
+        success, frame = vidcap.read() # get next frame from video
+        if cur_frame % frame_skip == 0: # only analyze every n=300 frames
+            print('frame: {}'.format(cur_frame)) 
+            pil_img = Image.fromarray(frame) # convert opencv frame (with type()==numpy) into PIL Image
+            st.image(pil_img)
+        cur_frame += 1
